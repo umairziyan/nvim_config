9dc1aa0256e603c1705a1c88f72e2dbbecd37098
umairziyan <umairziyan@protonmail.com>
2024-03-31 13:49:24 +0100

    added debugger

---
+++ lua/umairziyan/lazy/debugger.lua
@@ -0,0 +1,32 @@
+return {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+        "rcarriga/nvim-dap-ui",
+        "leoluz/nvim-dap-go",
+        "nvim-neotest/nvim-nio",
+        "mfussenegger/nvim-dap-python",
+    },
+    config = function()
+        local dap, dapui = require("dap"), require("dapui")
+
+        require("dap-go").setup()
+        require("dapui").setup()
+        require("dap-python").setup("~/.venvs/debugpy/bin/python")
+
+        dap.listeners.before.attach.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.launch.dapui_config = function()
+            dapui.open()
+        end
+        dap.listeners.before.event_terminated.dapui_config = function()
+            dapui.close()
+        end
+        dap.listeners.before.event_exited.dapui_config = function()
+            dapui.close()
+        end
+
+        vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
+        vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+    end,
+}
