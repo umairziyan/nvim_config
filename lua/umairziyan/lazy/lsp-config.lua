53c7062a756ff899bc98caf2a14263be21c3e157
umairziyan <umairziyan@protonmail.com>
2024-03-31 12:36:42 +0100

    added lsp snippets

--- lua/umairziyan/lazy/lsp-config.lua
+++ lua/umairziyan/lazy/lsp-config.lua
@@ -16,10 +16,11 @@ return {
     {
         "neovim/nvim-lspconfig",
         config = function ()
+            local capabilities = require('cmp_nvim_lsp').default_capabilities()
             local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({})
-            lspconfig.tsserver.setup({})
-            lspconfig.pyright.setup({})
+            lspconfig.lua_ls.setup({capabilities = capabilities})
+            lspconfig.tsserver.setup({capabilities = capabilities})
+            lspconfig.pyright.setup({capabilities = capabilities})
             -- Global mappings.
 
             -- See `:help vim.diagnostic.*` for documentation on any of the below functions
