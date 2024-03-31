c4a299fc03e9dbec638fce16b35c46a75cabf3b3
umairziyan <umairziyan@protonmail.com>
2024-03-31 14:01:21 +0100

    final additions

--- lua/umairziyan/lazy/theme.lua
+++ lua/umairziyan/lazy/theme.lua
@@ -1,63 +1,44 @@
 -- return {
 
--- "catppuccin/nvim", 
--- name = "catppuccin", 
+-- "catppuccin/nvim",
+-- name = "catppuccin",
 -- priority = 1000,
--- 
+--
 -- config =  function()
 --     vim.cmd.colorscheme "catppuccin"
 -- end
 -- }
 
 function ColorMyPencils(color)
-	color = color or "rose-pine"
-	vim.cmd.colorscheme(color)
+    color = color or "rose-pine"
+    vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
+    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
     {
         "folke/tokyonight.nvim",
 
+        "catppuccin/nvim",
         config = function()
-
-            require("tokyonight").setup({
-                -- your configuration comes here
-                -- or leave it empty to use the default settings
-
-                style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-                transparent = true, -- Enable this to disable setting the background color
-                terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-                styles = {
-                    -- Style to be applied to different syntax groups
-                    -- Value is any valid attr-list value for `:help nvim_set_hl`
-                    comments = { italic = false },
-                    keywords = { italic = false },
-                    -- Background styles. Can be "dark", "transparent" or "normal"
-                    sidebars = "dark", -- style for sidebars, see below
-                    floats = "dark", -- style for floating windows
-                },
-            })
-        end
-
+            require("catppuccin").setup()
+       end,
     },
 
     {
         "rose-pine/neovim",
         name = "rose-pine",
         config = function()
-            require('rose-pine').setup({
+            require("rose-pine").setup({
                 disable_background = true,
             })
 
-            vim.cmd("colorscheme rose-pine")
+            vim.cmd("colorscheme catppuccin")
+
 
             ColorMyPencils()
-        end
+        end,
     },
-
-
 }
