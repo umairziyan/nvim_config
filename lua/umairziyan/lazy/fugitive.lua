244acbb2ab556c460dc5e13945a9a51356b3e94f
umairziyan <umairziyan@protonmail.com>
2024-03-31 14:21:57 +0100

    added git functionality

--- lua/umairziyan/lazy/fugitive.lua
+++ lua/umairziyan/lazy/fugitive.lua
@@ -1,1 +1,9 @@
-return {}
+return {
+	"lewis6991/gitsigns.nvim",
+	"tpope/vim-fugitive",
+	config = function()
+		require("gitsigns").setup()
+
+		vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+	end,
+}
