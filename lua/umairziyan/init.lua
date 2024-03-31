4bab04c180e650e8372e7958ebc4a25c2bf8f5d7
umairziyan <umairziyan@protonmail.com>
2024-03-31 00:35:02 +0000

    Added lsp configuration

--- lua/umairziyan/init.lua
+++ lua/umairziyan/init.lua
@@ -1,3 +1,24 @@
 require("umairziyan.remap")
 require("umairziyan.set")
-require("lazy_init")
+-- require("lazy_init")
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+
+-- Use a folder to do all manipulation
+vim.opt.rtp:prepend(lazypath)
+require("lazy").setup({
+    spec = "umairziyan.lazy",
+    change_detection = { notify = false }
+})
+
--- lua/umairziyan/lazy/telescope.lua
+++ lua/umairziyan/lazy/telescope.lua
@@ -1,4 +1,5 @@
 return {
+    {
     "nvim-telescope/telescope.nvim",
 
     tag = "0.1.5",
@@ -30,5 +31,19 @@ return {
         vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
 
     end
-
+    },
+    {
+        'nvim-telescope/telescope-ui-select.nvim',
+        config = function()
+        require("telescope").setup {
+            extensions = {
+                ["ui-select"] = {
+                    require("telescope.themes").get_dropdown {
+                    }
+                }
+            }
+        }
+        require("telescope").load_extension("ui-select")
+        end
+    }
 }
